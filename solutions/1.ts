bd71026005d24d7a11815374c8ad0c231163f706
ziqfaiz <haziqfaiz.ripin@gmail.com>
2024-02-29 20:07:44 +0800

    twosum

--- solutions/1.ts
+++ solutions/1.ts
@@ -19,6 +19,25 @@ let solution = (arr: number[], target: number): (number|undefined)[] => {
     return [];
 }
 
+let solution2 = (arr: number[], target: number): (number|undefined)[] => {
+    let map = new Map<number, number>();
+
+    arr.forEach((element, index) => {
+        if (map.has(element)){
+            return [index, map.get(element)];
+            
+        }
+        else{
+            map.set(target-element, index);
+        }
+        
+    });
+
+    console.log(map);
+    return [];
+}
+
+
 let arr: number[] = [2,7,11,15];
 
-console.log(solution(arr,9));
+console.log(solution2(arr,9));
